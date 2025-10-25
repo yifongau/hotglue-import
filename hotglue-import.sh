7525015c16ec8ebc4cca85c9412b973e096f02df
Yi Fong Au <yifongau@gmail.com>
2025-10-25 20:11:20 +0200

    do images as well

--- hotglue-import.sh
+++ hotglue-import.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 xBaseName() {
 
 	baseName="$1"
@@ -13,18 +12,20 @@ xBaseName() {
 
 }
 
-# different handlers for different filetypes
 hrefToFileName() {
 	url=$1
-	
+
+	# referenced icon files
 	if grepString=$(echo "$url" | grep -Po "img\/.*?\.ico"); then
 		fileName=$grepString
 		echo $fileName
 
+	# referenced css files
 	elif grepString=$(echo "$url" | grep -Po "css\/.*?\.css"); then
 		fileName=$grepString
 		echo $fileName
-
+	
+	# html files in query string format
 	elif grepString=$(echo "$url" | grep -Po "\?.*?$"); then
 		fileName=$(xBaseName $( echo $grepString | tr -d '/') "?" ".html" )
 		echo $fileName
@@ -32,30 +33,43 @@ hrefToFileName() {
 
 }
 
-# different handlers for different filetypes
-imgSrcHandler() {
+imgSrctoFileName() {
 	url=$1
 
-	# different handlers for different filetypes
-	if filename=$(echo "$url" | grep -Po "css\/.*?\.css") ; then
-		curl $url --create-dirs --output ./"$filename"	
-
+	# image files in query string format, assuming .png format
+	if grepString=$(echo "$url" | grep -Po "\?.*?$"); then
+		fileName=$(xBaseName $( echo $grepString | tr -d '/') "?" ".png")
+		echo "img/$fileName"
 	fi
 
 }
 
-updateHrefs() {
-	file=$1
 
-
-}
-
+# This is the big recursive function
 getLinkedFiles() {
 	parentFileName=$1
 
 	# parse the resulting file and grep url string of linked files
-#	imgSrcList=$(cat ./$parentFileName | hxclean | hxselect img | grep -Po "\"https:\/\/.*?\"")
+	imgSrcList=$(cat ./$parentFileName | hxclean | hxselect img | grep -Po "\"https:\/\/.*?\"")
 	hrefList=$(cat ./$parentFileName | hxclean | hxselect [href] | grep -Po "\"https:\/\/.*?\"")
+	htmlFiles=()
+
+	# iterate over image sources and retrieve them
+	while IFS= read -r href ; do 
+
+		# sanitize URL of linked file
+		childUrl=$( echo $href | tr -d '"')
+		childFileName=$(imgSrcToFileName $childUrl)
+
+		# curl the childUrl and save to filename
+		if test -f ./$childFileName; then
+			echo "$childFileName already exists, doing nothing"
+		else
+			sleep 1
+			curl "$childUrl" --create-dirs --output ./$childFileName
+		fi
+
+	done <<< "$imgSrcList"
 
 	# iterate over hrefs and retrieve them
 	while IFS= read -r href ; do 
@@ -68,22 +82,30 @@ getLinkedFiles() {
 		if test -f ./$childFileName; then
 			echo "$childFileName already exists, doing nothing"
 		else
-			sleep 100
+			sleep 1
 			curl "$childUrl" --create-dirs --output ./$childFileName
 		fi
 
 
-		# Recurse into .html files, but not if they have already been recursed
-		alreadyRecursed+=($parentFileName)
-		echo $alreadyRecursed
+		# Remember any files ending in .html
+		if echo $childFileName | grep ".html" > /dev/null; then
+			htmlFiles+=($childFileName)
 
-		if echo $childFileName | grep ".html"; then
-			if [[ ! ${alreadyRecursed[@]} =~ $childFileName ]]; then
-				getLinkedFiles "$childFileName"
-			fi
 		fi
 
 	done <<< "$hrefList"
+	
+	# linked files have now been retrieved, add parent to alreadyRecursed
+	alreadyRecursed+=($parentFileName)
+
+	# iterate over any .html files, and recurse into them if not already recursed
+	while IFS= read -r htmlFile ; do 
+
+		if [[ ! ${alreadyRecursed[@]} =~ $childFileName ]]; then
+			getLinkedFiles "$childFileName"
+
+		fi
+	done <<< "$htmlFiles"
 
 }
 
